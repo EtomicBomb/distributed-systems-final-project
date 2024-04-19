7fad5a9466f2cb36d084e7a805caaf4628159716
EtomicBomb <ethan@ethan.ws>
2024-04-18 22:38:22 -0400

    remove commented code

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -13,26 +13,11 @@ aws ec2 describe-instances --filters "Name=instance.group-id,Values=$SG_INTERNAL
     | jq '[.Reservations[].Instances[] | {public: .NetworkInterfaces[0].Association.PublicIp, private: .PrivateIpAddress}]' \
     > internal.json
 
-# # gather the ips
-# aws ec2 describe-instances --filters "Name=instance.group-id,Values=$SG_CLIENT" --output json \
-#     | jq '[.Reservations[].Instances[].NetworkInterfaces[0].Association.PublicIp]' \
-# aws ec2 describe-instances --filters "Name=instance.group-id,Values=$SG_CLIENT" --output json \
-#     | jq '[.Reservations[].Instances[].PrivateIpAddress]' \
-#     > client-private.json
-# aws ec2 describe-instances --filters "Name=instance.group-id,Values=$SG_INTERNAL" --output json \
-#     | jq '[.Reservations[].Instances[].PrivateIpAddress]' \
-#     > internal-private.json
-# aws ec2 describe-instances --filters "Name=instance.group-id,Values=$SG_INTERNAL" --output json \
-#     | jq '[.Reservations[].Instances[].NetworkInterfaces[0].Association.PublicIp]' \
-#     > internal-public.json
-
 run_ssh() {
     ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i keypair-1380.pem "admin@$1" "$2"
 }
 
 # ssh -i keypair-1380.pem "admin@$ip"
-
-# no trailing comma allowed
 
 for ip in $(jq -nc --argfile internal internal.json --argfile client client.json '$internal + $client | .[]'); do
     targetPublic=$(echo "$ip" | jq -r '.public')
@@ -59,9 +44,7 @@ EOL
     b=$(jq -n --arg targetPrivate "$targetPrivate" --argfile client client.json --argfile internals internal.json "$a")
     known=$(node -e "console.log(require('../distribution/util/serialization.js').serialize($b))")
 
-    #run_ssh $targetPublic "sudo apt update && sudo apt install -y nodejs git vim npm && mkdir -p final && rm -rf final/distribution"
-    run_ssh $targetPublic "mkdir -p final && rm -rf final/distribution"
+    run_ssh $targetPublic "sudo apt update && sudo apt install -y nodejs git vim npm && mkdir -p final && rm -rf final/distribution"
     scp -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i keypair-1380.pem -r ../distribution.js ../distribution ../package.json ../data ../store "admin@$targetPublic:~/final"
-#     run_ssh $targetPublic "cd final; npm install; pkill node; nohup ./distribution.js --foo 4 --config '$known' >> log.txt 2>&1 &"
-    run_ssh $targetPublic "cd final; pkill node; nohup ./distribution.js --foo 4 --config '$known' >> log.txt 2>&1 &"
+    run_ssh $targetPublic "cd final; npm install; pkill node; nohup ./distribution.js --foo 4 --config '$known' >> log.txt 2>&1 &"
 done
